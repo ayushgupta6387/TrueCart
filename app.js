65fbfab5ef263fdf6cd8bf139c74ff4c752c1089
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-03-22 22:55:22 +0530

    Remove warnings

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ require("dotenv").config();
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 }).then(()=> console.log("DATABASE CONNECTED"));
 
 
