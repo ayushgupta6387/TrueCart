e65c229a8e3c4d5a7c5964e6769b0910cc924f30
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-03-23 17:14:41 +0530

    Require sign, signup, signout from controllers

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup } = require('../controllers/user');
+const { signup, signin, signout } = require('../controllers/user');
 const { userSignupValidator } = require('../validate/index');
 
+// For sign up a new user
 router.post('/signup', userSignupValidator, signup);
 
+// For signing in
+router.post('/signin', signin);
 // this is written before using controllers
 // router.get("/", (req,res)=>{
 //     res.send("Hello ABCD")
 // });
+
+// For signing out 
+router.get('/signout', signout);
 
 module.exports = router;
