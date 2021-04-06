5ec75951cf3f583fa17a5f53d5651ad0dedc13d4
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-04-06 18:19:10 +0530

    Create product route

---
+++ routes/product.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { create } = require('../controllers/category');
+const { requireSign, isAuth, isAdmin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+
+router.post('/product/create/:userId', requireSign, isAuth, isAdmin, create);
+
+// For sign up a new user
+router.param('userId', userById);
+
+module.exports = router;
