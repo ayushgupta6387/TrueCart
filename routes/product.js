25818326e4a27a5bb5e9dbb2c47b7693d37dbcf1
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-04-10 18:10:28 +0530

    fix product.js error

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category');
+const { create } = require('../controllers/product');
 const { requireSign, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
-router.post('/product/create/:userId', requireSign, isAuth, isAdmin, create);
+router.post('/product/create/:userId', create);
 
 // For sign up a new user
 router.param('userId', userById);
