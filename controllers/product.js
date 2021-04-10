cb1542f608e46d65929529eaf6076ee7f48a22ed
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-04-10 18:10:58 +0530

    Create product create method

---
+++ controllers/product.js
@@ -0,0 +1,56 @@
+const formidable = require('formidable');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const Product = require('../models/product');
+
+const { errorHandler } = require('../helpers/dbErrorHandler');
+
+exports.create = (req, res) => {
+	const form = new formidable.IncomingForm(); // for getting image from the user
+	form.keepExtensions = true;
+	form.parse(req, (err, fields, files) => {
+		if (err) {
+			return res.status(400).json({
+				error: 'Image could not be uploaded',
+			});
+		}
+
+	
+		//check for all the fields
+		const { name, description, price, category, quantity, shipping } = fields;
+		if (!name || !description || !price || !category || !quantity || !shipping) {
+			return res.status(400).json({
+				error: 'All fields are required',
+			});
+		}
+
+		let product = new Product(fields);
+
+		// 1kb = 1000
+		// 1mb = 1000000
+
+		if (files.photo) {
+			//console.log("Files photo", files.photo) //Checking file size
+
+			if (files.photo.size > 1000000) {
+				return res.status(400).json({
+					error: 'Image should be less then 1mb',
+				});
+			}
+			product.photo.data = fs.readFileSync(files.photo.path);
+			product.photo.contentType = files.photo.type;
+		}
+
+		product.save((err, result) => {
+			if (err) {
+				res.status(400).json({
+					error: errorHandler(err),
+				});
+			}
+
+			res.json(result);
+		});
+	});
+};
