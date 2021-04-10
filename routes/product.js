3b0ac19f2784a2490ff7c9daf2749c56333d7f87
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-04-10 23:51:16 +0530

    Add remove, update, read methods

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/product');
+const { create, productById, read, remove, update, list, listRelated} = require('../controllers/product');
 const { requireSign, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
-router.post('/product/create/:userId', create);
+router.get('/product/:productId', read);
+router.post('/product/create/:userId', requireSign, isAuth, isAdmin, create);
+
+router.delete('/product/:productId/:userId', requireSign, isAuth, isAdmin, remove);
+router.put("/product/:productId/:userId", requireSign, isAuth, isAdmin, update);
+
+router.get("/products", list);
+router.get("/products/realed/:productId", listRelated);
 
 // For sign up a new user
 router.param('userId', userById);
+router.param('productId', productById);
+
 
 module.exports = router;
