3189c7b28592956e6fb5cc3aff7ef031d9e3817e
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-04-11 01:10:59 +0530

    Add more usable method in products.js

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update, list, listRelated} = require('../controllers/product');
+const {
+	create,
+	productById,
+	read,
+	remove,
+	update,
+	list,
+	listRelated,
+	listCategories,
+	listBySearch,
+	photo,
+} = require('../controllers/product');
 const { requireSign, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
@@ -9,14 +20,16 @@ router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSign, isAuth, isAdmin, create);
 
 router.delete('/product/:productId/:userId', requireSign, isAuth, isAdmin, remove);
-router.put("/product/:productId/:userId", requireSign, isAuth, isAdmin, update);
+router.put('/product/:productId/:userId', requireSign, isAuth, isAdmin, update);
 
-router.get("/products", list);
-router.get("/products/realed/:productId", listRelated);
+router.get('/products', list);
+router.get('/products/related/:productId', listRelated);
+router.get('/products/categories', listCategories);
+router.post('/products/by/search', listBySearch);
+router.get('/product/photo/:productId', photo);
 
 // For sign up a new user
 router.param('userId', userById);
 router.param('productId', productById);
-
 
 module.exports = router;
