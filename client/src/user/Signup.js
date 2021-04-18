f21c1a207c10bb7b4d1041684a467a2198873419
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-04-18 17:48:45 +0530

    Use the useState to set the values

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Layout from '../core/Layout';
 
 const Signup = () => {
 
+const [values, setValues] = useState({
+	name:'',
+	email:'',
+	password:'',
+	error:'',
+	success: false
+})
+
+const handleChange = name => event => {
+	setValues({ ...values, error: false, [name]: event.target.value });
+};
+
+
 	const signUpForm = () =>(
 		<form>
 			<div className="form-group">
 				<label className="text-muted">Name</label>
-				<input type="text" className="form-control"/>
+				<input onChange={handleChange('name')} type="text" className="form-control"/>
 			</div>
 
 			<div className="form-group">
 				<label className="text-muted">Email</label>
-				<input type="email" className="form-control"/>
+				<input onChange={handleChange('email')} type="email" className="form-control"/>
 			</div>
 
 			<div className="form-group">
 				<label className="text-muted">Password</label>
-				<input type="password" className="form-control"/>
+				<input onChange={handleChange('password')} type="password" className="form-control"/>
 			</div>
 
 			<button className="btn btn-primary">Submit</button>
@@ -29,6 +42,7 @@ const Signup = () => {
 	return (
 		<Layout title="SignupPage"
 		 description="Node React E-commerce App"
+		 className="container col-md-8 offset-md-2"
 		>
 		{signUpForm()}
 		</Layout>
