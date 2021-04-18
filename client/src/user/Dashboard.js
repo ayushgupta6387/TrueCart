97001b5be633cf0750afd22b183ee351195a0b1d
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-04-18 22:04:38 +0530

    Show purchase history method

--- client/src/user/Dashboard.js
+++ client/src/user/Dashboard.js
@@ -29,31 +29,48 @@ const Dashboard = () => {
         );
     };
 
+    const userInfo = () => {
+        return (
+            <div className="card mb-5">
+                <h3 className="card-header">User Information</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">{name}</li>
+                    <li className="list-group-item">{email}</li>
+                    <li className="list-group-item">
+                        {role === 1 ? "Admin" : "Registered User"}
+                    </li>
+                </ul>
+            </div>
+        );
+    };
+
+    
+    const purchaseHistory = () => {
+        return (
+            <div className="card mb-5">
+                <h3 className="card-header">Purchase history</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">history</li>
+                </ul>
+            </div>
+        );
+    };
+
 
     return (
-        <Layout
-        title="Dashboard"
-        description="User Dashboard"
-        className="container"
-    >
-        <div className="card mb-5">
-            <h3 className="card-header">User Information</h3>
-            <ul className="list-group">
-                <li className="list-group-item">{name}</li>
-                <li className="list-group-item">{email}</li>
-                <li className="list-group-item">
-                    {role === 1 ? "Admin" : "Registered User"}
-                </li>
-            </ul>
-        </div>
-
-        <div className="card mb-5">
-            <h3 className="card-header">Purchase history</h3>
-            <ul className="list-group">
-                <li className="list-group-item">history</li>
-            </ul>
-        </div>
-    </Layout>
+               <Layout
+            title="Dashboard"
+            description={`G'day ${name}!`}
+            className="container-fluid"
+        >
+            <div className="row">
+                <div className="col-3">{userLinks()}</div>
+                <div className="col-9">
+                    {userInfo()}
+                    {purchaseHistory()}
+                </div>
+            </div>
+        </Layout>
     );
 };
 
