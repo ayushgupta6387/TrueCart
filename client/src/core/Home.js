aa88cdac91c7d8b9d10efe30c27cf1e90d1b7746
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-04-18 14:49:20 +0530

    Create a component Home.js in core folder

---
+++ client/src/core/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout  from './Layout';
+
+const Home = () => {
+	return (
+		<Layout title="HomePage" description="Node React E-commerce App">
+			...
+		</Layout>
+	);
+};
+
+export default Home;
