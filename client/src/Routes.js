712091933ed05bb6eff12a196b9173c4bfaadabf
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-04-18 22:13:56 +0530

    Create method for admin

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -7,7 +7,7 @@ import Signup from './user/Signup';
 import Signin from './user/Signin';
 import Home from './core/Home';
 import PrivateRoute from './auth/PrivateRoute';
-import Dashboard from './user/Dashboard';
+import Dashboard from './user/UserDashboard';
 
 const Routes = () => { 
 	return (
