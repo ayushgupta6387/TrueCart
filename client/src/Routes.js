2f0f606a7bcb3a39372a3c4c0e377c38f8535f0a
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-04-19 18:32:53 +0530

    Use AddCategory method in routes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -10,6 +10,7 @@ import PrivateRoute from './auth/PrivateRoute';
 import Dashboard from './user/UserDashboard';
 import AdminRoute from "./auth/AdminRoute";
 import AdminDashboard from "./user/AdminDashboard";
+import AddCategory from "./admin/AddCategory";
 
 const Routes = () => { 
 	return (
@@ -23,6 +24,11 @@ const Routes = () => {
                     path="/admin/dashboard"
                     exact
                     component={AdminDashboard}
+                />
+				<AdminRoute
+                    path="/create/category"
+                    exact
+                    component={AddCategory}
                 />
 			</Switch>
 		</BrowserRouter>
