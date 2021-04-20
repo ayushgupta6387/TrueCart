4103189836614dcdffe5ca8c1d56d5c670985961
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-04-20 15:46:08 +0530

    Show the product image in the home page

--- client/src/core/Card.js
+++ client/src/core/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import ShowImage from './ShowImage';
 
 
 // to show the product on home page
@@ -10,6 +10,7 @@ const Card = ({ product }) => {
             <div className="card">
                 <div className="card-header">{product.name}</div>
                 <div className="card-body">
+                <ShowImage item={product} url="product"/>
                     <p>{product.description}</p>
                     <p>${product.price}</p>
                     <Link to="/">
