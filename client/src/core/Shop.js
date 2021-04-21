eac5d69328b3a07361f6767c8f35fdc432560457
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-04-21 14:47:52 +0530

    Create load more button

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -18,6 +18,7 @@ const Shop = () => {
     const [error, setError] = useState(false);
     const [limit, setLimit] = useState(6);
     const [skip, setSkip] = useState(0);
+    const [size, setSize] = useState(0);
     const [filteredResults, setFilteredResults] = useState([]);
 
 
@@ -38,11 +39,38 @@ const loadFilteredResults = newFilters => {
         if (data.error) {
             setError(data.error);
         } else {
-            setFilteredResults(data);
+            setFilteredResults(data.data);
+            setSize(data.size);
+            setSkip(0);
         }
     });
 };
 
+const loadMore = () => {
+    let toSkip = skip + limit;
+    // console.log(newFilters);
+    getFilteredProducts(toSkip, limit, myFilters.filters).then(data => {
+        if (data.error) {
+            setError(data.error);
+        } else {
+            setFilteredResults([...filteredResults, ...data.data]);
+            setSize(data.size);
+            setSkip(toSkip);
+        }
+    });
+};
+
+
+const loadMoreButton = () => {
+    return (
+        size > 0 &&
+        size >= limit && (
+            <button onClick={loadMore} className="btn btn-warning mb-5">
+                Load more
+            </button>
+        )
+    );
+};
 
 
     useEffect(()=>{
@@ -111,7 +139,15 @@ const loadFilteredResults = newFilters => {
                 </div>
 
                 <div className="col-8">
-                    {JSON.stringify(filteredResults)}
+                    {/* {JSON.stringify(filteredResults)} */}
+                    <h2 className="mb-4">Products</h2>
+                    <div className="row">
+                        {filteredResults.map((product, i) => (
+                            <Card key={i} product={product} />
+                        ))}
+                    </div>
+                    <hr/>
+                    {loadMoreButton()}
                 </div>
             </div>
         </Layout>
