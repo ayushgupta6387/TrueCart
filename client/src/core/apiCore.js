86ed4181c144275dd578ef3d885b8e4b37be355c
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-04-21 22:36:23 +0530

    Show payment UI on the user page

--- client/src/core/apiCore.js
+++ client/src/core/apiCore.js
@@ -84,3 +84,20 @@ export const listRelated = productId => {
         })
         .catch(err => console.log(err));
 };
+
+// get braintree user token
+
+export const getBraintreeClientToken = (userId, token) => {
+    return fetch(`${API}/braintree/getToken/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
