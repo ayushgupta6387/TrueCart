af119d60a7c3b936a1512f7bb3573cf5703fd409
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-04-22 14:37:06 +0530

    Manage status of order by admin

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -28,3 +28,8 @@ exports.listOrders = (req, res) => {
             res.json(orders);
         });
 };
+
+// enums
+exports.getStatusValues = (req, res) => {
+    res.json(Order.schema.path('status').enumValues);
+};
--- server/routes/order.js
+++ server/routes/order.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { requireSign, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create, listOrders } = require("../controllers/order");
+const { create, listOrders, getStatusValues } = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.post("/order/create/:userId", requireSign, 
@@ -13,6 +13,16 @@ decreaseQuantity,
 create);
 
 router.get("/order/list/:userId", requireSign, isAuth, isAdmin, listOrders);
+
+router.get(
+    "/order/status-values/:userId",
+    requireSign,
+    isAuth,
+    isAdmin,
+    getStatusValues
+);
+
+
 
 router.param("userId", userById);
 
