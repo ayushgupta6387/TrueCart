69f1f868b7727d24de5937606e286abdec03d3e4
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2021-04-22 13:01:07 +0530

    Increament Decreament product quantity

--- server/routes/order.js
+++ server/routes/order.js
@@ -4,8 +4,13 @@ const router = express.Router();
 const { requireSign, isAuth } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
 const { create } = require("../controllers/order");
+const { decreaseQuantity } = require("../controllers/product");
 
-router.post("/order/create/:userId", requireSign, isAuth, addOrderToUserHistory, create);
+router.post("/order/create/:userId", requireSign, 
+isAuth, 
+addOrderToUserHistory, 
+decreaseQuantity,
+create);
 
 router.param("userId", userById);
 
